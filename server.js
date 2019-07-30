7ffa391b99efcf6f89b7f1cbc803ca8e9d408a58
Phong <phongit1995@gmail.com>
2019-07-30 20:35:49 +0700

    lần 2 commit

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ app.set('view engine', 'ejs');
 app.get("/",(req,res)=>{
     res.render('login');
 })
+
+app.get("/Buoc-1",(req,res)=>{
+    res.render("buoc1");
+})
+app.post("/xuly",(req,res)=>{
+    res.redirect("/Buoc-1");
+})
+
+
 app.listen(8000,()=>{
     console.log("APp đang chạy trên port 8000");
 })
