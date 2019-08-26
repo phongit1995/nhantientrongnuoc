12a6c82db76fd6d005fa8d06028f787051f4cd9e
Phong <phongit1995@gmail.com>
2019-08-26 21:07:01 +0700

    hoàn thành xong đơn nhận tiền

--- routers/admin.js
+++ routers/admin.js
@@ -182,9 +182,29 @@ router.post("/them-ngan-hang",(req,res)=>{
 })
 // confirm transfer
 router.post("/confirm-transfer",(req,res)=>{
+    
+    req.body.numbermoney = req.body.numbermoney.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+    req.body.name = xoa_dau(req.body.name);
     console.log(req.body);
     res.render("admin/confirmtranfer",{data:req.body});
 })
+function xoa_dau(str) {
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+  str = str.replace(/đ/g, "d");
+  str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+  str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+  str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+  str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+  str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+  str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+  str = str.replace(/Đ/g, "D");
+  return str;
+}
 // router.get("/tao",(req,res)=>{
 //   User.create({username:"admin",password:"admin"});
 // })
